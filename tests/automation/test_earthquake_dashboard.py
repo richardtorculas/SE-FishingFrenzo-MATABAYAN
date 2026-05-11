f139ffbfd94c3f64e7bff860db667545f9d04bea
Richard Torculas <richardtorculas712@gmail.com>
2026-05-11 23:12:09 +0800

    test: earthquake dashboard

--- tests/automation/test_earthquake_dashboard.py
+++ tests/automation/test_earthquake_dashboard.py
@@ -4,4 +4,4 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# All earthquake dashboard tests removed - no stable tests remaining
+# Earthquake dashboard tests
